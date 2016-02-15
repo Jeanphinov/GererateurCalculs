ede4e9f5ea1cdff795fea91eb19504fc0b3769d9
Jeanphi <novitz@gmail.com>
2016-02-15 12:43:56 +0100

    ...

--- view/vue-form.php
+++ view/vue-form.php
@@ -17,18 +17,21 @@
 </nav>
 
 <div class="container">
-    <form action="index.php" method="POST" class="form form-horizontal col-lg-6">
+<div class="row">
+          <div class="col-sm-12">
+    <form action="index.php" method="POST" class="form-horizontal">
         <fieldset>
             <legend>Quelques calculs...</legend>
             <div class="form-group">
+			
             <?php
 
             foreach ($operations as $oper => $valeurs) {
                 ?>
 
                     <label for='reponses[<?php echo $oper ?>]'
-                           class="col-lg-2 control-label"> <?php echo $oper . " = " ?> </label>
-                    <div class="col-lg-2"><input type='text' size='2' name='reponses[<?php echo $oper ?>]'
+                           class="col-sm-2 control-label"> <?php echo $oper . " = " ?> </label>
+                    <div class="col-sm-2"><input type='text' size='2' name='reponses[<?php echo $oper ?>]'
                                                  id='reponses[<?php echo $oper ?>]' value='' class="form-control"></div>
 
                 <?php
@@ -40,6 +43,8 @@
                 <input type="hidden" name="calcul" value='<?php echo base64_encode(serialize($calcul)) ?>'>
                 <input type="submit" name="envoi" class="col-lg-offset-2 btn btn-primary">
             </div>
+			</div>
+			</div>
 
         </fieldset>
     </form>
